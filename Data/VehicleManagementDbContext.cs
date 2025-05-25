e1ab14e438eb09f1b314b729d66029268b9d816c
teo-mateo <teomati@gmail.com>
2025-05-25 07:44:59 +0200

    Checkpoint 2025-05-25T07:44:46+02:00

---
+++ Data/VehicleManagementDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data
+{
+    public class Vehicle
+    {
+        public long Id { get; set; }
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string LicensePlate { get; set; } = string.Empty;
+        [Timestamp]
+        public byte[] Timestamp { get; set; } = null!;
+    }
+
+    public class VehicleManagementDbContext : DbContext
+    {
+        public VehicleManagementDbContext(DbContextOptions<VehicleManagementDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; } = null!;
+    }
+}
